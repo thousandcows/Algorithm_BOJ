b8198c08537c672d5655f23909623cefd34156cb
thousandcows <90372024+thousandcows@users.noreply.github.com>
2021-11-09 08:27:43 +0900

    16진수 -> 10진수

---
+++ src/N_1550_16진수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class N_1550_16진수 {
+	
+	public static void main(String[] args) {
+		
+		// import java.util.Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// 16진수 받아 10진수로 출력
+		int hexNum = scan.nextInt(16);
+		System.out.println(hexNum);
+		
+	}
+}
