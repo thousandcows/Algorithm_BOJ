007b52c616e55f2bf1d582996219271635c5040a
thousandcows <90372024+thousandcows@users.noreply.github.com>
2021-11-09 14:58:33 +0900

    배열 적절히 활용하기

--- src/N_10250_ACM호텔.java
+++ src/N_10250_ACM호텔.java
@@ -10,7 +10,7 @@ public class N_10250_ACM호텔 {
 		// 테스트 케이스 받기
 		int testCase = scan.nextInt();
 		
-		// 
+		// 객실 번호 출력하기
 		for(int i = 0; i < testCase; i++) {
 			
 			// H, W, N 값 받기
@@ -25,6 +25,7 @@ public class N_10250_ACM호텔 {
 		}
 	}
 
+	// 객실정보 생성 메서드
 	private static int getRoomNumber(int height, int width, int guestNumber) {
 		
 		// 총 객실 수 길이 만큼의 배열 생성
