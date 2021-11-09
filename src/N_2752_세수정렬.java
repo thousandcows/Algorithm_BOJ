4c633722ed809f4a5626693bc5958dba62ffb07e
thousandcows <90372024+thousandcows@users.noreply.github.com>
2021-11-10 08:36:12 +0900

    bubblesort 활용해서 숫자 출력하기

--- src/N_2752_세수정렬.java
+++ src/N_2752_세수정렬.java
@@ -4,22 +4,25 @@ public class N_2752_세수정렬 {
 
 	public static void main(String[] args) {
 
+		// import java.util.Scanner
 		Scanner scan = new Scanner(System.in);
-
+		
+		// 길이 3이 배열에 각 값을 저장
 		int [] intArr = new int[3];
 		intArr[0] = scan.nextInt();
 		intArr[1] = scan.nextInt();
 		intArr[2] = scan.nextInt();
-
+		
+		// 오름차순으로 정렬
 		sort(intArr);
-
+		
+		// 정렬된 새 배열 출력
 		for(int i = 0; i < 3; i++) {
 			System.out.print(intArr[i] + " ");
 		}
-
-
 	}
-
+	
+	// 오름차순 정렬 메서드
 	private static void sort(int [] array) {
 		int tmp = 0;
 		for(int i = 0; i < 2; i++) {
