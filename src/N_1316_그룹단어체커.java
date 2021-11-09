39ff0746ed191ca761da0895b92de19331c31701
thousandcows <90372024+thousandcows@users.noreply.github.com>
2021-11-09 14:05:40 +0900

    배열 활용해서 연속된 문자 다루기

---
+++ src/N_1316_그룹단어체커.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class N_1316_그룹단어체커 {
+
+	public static void main(String[] args) {
+		
+		// import java.util.Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// 단어 개수 입력받기
+		int wordNum = Integer.parseInt(scan.nextLine());
+		
+		// 그룹단어 개수 출력하기
+		int count = 0;
+		
+		for(int i = 0; i < wordNum; i ++) {
+			
+			String word = scan.nextLine();
+			
+			if(isGroupWord(word)) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+
+	private static boolean isGroupWord(String word) {
+		
+		boolean test = true;
+		
+		String [] arr = word.split("");
+
+		for (int i = 1; i < arr.length-1; i++) {
+			
+			if(i == arr.length) break;
+			
+			if (!arr[i+1].equals(arr[i])) {
+				for(int j = 0; j < i+1; j++) {
+					if(arr[i+1].equals(arr[j])) {
+						test = false;
+						break;
+					}
+				}
+			}
+		}
+		return test;
+	}
+}
