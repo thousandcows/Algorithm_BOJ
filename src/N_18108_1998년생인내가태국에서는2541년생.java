62baf9af2ae4116aacccbeed884db15fa3c178fe
thousandcows <90372024+thousandcows@users.noreply.github.com>
2021-11-12 14:34:42 +0900

    불기 -> 서기 단위로 출력

---
+++ src/N_18108_1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class N_18108_1998년생인내가태국에서는2541년생 {
+
+	public static void main(String[] args) {
+
+
+		// import java.util.scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// 불기 해 입력받기
+		int buddhaYear = scan.nextInt();
+		
+		// 서기 해 출력
+		System.out.println(buddhaYear - 543);
+	}
+
+}
