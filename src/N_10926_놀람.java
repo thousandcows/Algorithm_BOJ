62b1e50c28b76941424ee78c6ceb9160d22fa13e
thousandcows <90372024+thousandcows@users.noreply.github.com>
2021-11-18 17:55:26 +0900

    String 값 조건에 맞게 출력

---
+++ src/N_10926_놀람.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class N_10926_놀람 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String name = scan.next();
+		
+		System.out.println(name + "??!");
+		
+	}
+}
