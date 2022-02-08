5f53b267989c11d22ad29b4289b61999bdd69154
thousandcows <milcamboo@gmail.com>
2022-02-08 20:54:10 +0900

    commit again

--- src/N_11651.java
+++ src/N_11651.java
@@ -19,8 +19,8 @@ public class N_11651 {
 		int [][] arr = new int [N][2];
 		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
 		}
 		
 		// Sorting using Comparator
@@ -28,6 +28,9 @@ public class N_11651 {
 
 			@Override
 			public int compare(int[] o1, int[] o2) {
+				
+				System.out.println("o1 : " + o1[0] + " o1 : " + o1[1]);
+				System.out.println("o2 : " + o2[0] + " o2 : " + o2[1]);
 				
 				if(o1[1] == o2[1]) {
 					return o1[0] - o2[0];
