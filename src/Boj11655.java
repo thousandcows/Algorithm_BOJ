03008017635c796b1d3bd6807bdea5e50e867561
thousandcows <milcamboo@gmail.com>
2022-02-16 07:43:11 +0900

    Basic problem solving using ascii

---
+++ src/Boj11655.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj11655 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		// 1. Read the String
+		String sentence = br.readLine();
+
+		// 2. Do ROT13
+
+		for(int i = 0; i < sentence.length(); i++) {
+			// 65 ~ 90
+			// 97 ~ 122
+			char letter = sentence.charAt(i);
+			char newChar;
+
+			if((letter >= 65 && letter <= 90)) {
+
+				newChar = (char) (letter + 13);
+
+				if(newChar > 90) {
+					newChar = (char)(newChar - 26);
+				}
+
+				sb.append(newChar);
+
+			} else if((letter >= 97 && letter <= 122)) {
+
+				newChar = (char) (letter + 13);
+
+				if(newChar > 122) {
+					newChar = (char)(newChar - 26);
+				}
+
+				sb.append(newChar);
+			}
+			else {
+				sb.append(letter);
+			}
+		}
+
+		// 3. Print result
+		System.out.println(sb);
+	}
+
+
+}
