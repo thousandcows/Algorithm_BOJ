d6f1ba389c575e15844ee582ee850c5b3dbb76f0
thousandcows <milcamboo@gmail.com>
2022-02-25 10:52:47 +0900

    Baisc greedy algorithm

---
+++ src/Boj1789.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Boj1789 {
+	
+	public static void main(String[] args) {
+		
+		// 1. Read S
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int s = Integer.parseInt(br.readLine());
+		
+		// 2. Find max N
+		
+		// 3. Print N
+		
+	}
+
+}
