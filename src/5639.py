6093cef21e1d918113333e16255a9f86b2571ba9
thousandcows <milcamboo@gmail.com>
2022-09-07 15:24:48 +0900

    learned setting recursionlimit is crucial in memory management

---
+++ src/5639.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10 ** 3)
+
+# Graph: stack
+tree = []
+
+while True:
+    try:
+        tree.append(int(sys.stdin.readline()))
+
+    except:
+        break
+
+
+def postorder(start, end):
+    if start > end:
+        return
+
+    mid = end + 1
+
+    for i in range(start + 1, end + 1):
+        if tree[i] > tree[start]:
+            mid = i
+            break
+
+    postorder(start + 1, mid - 1)
+    postorder(mid, end)
+
+    print(tree[start])
+
+
+postorder(0, len(tree) - 1)
