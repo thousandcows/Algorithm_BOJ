cfb82d3518a2753028c68c9dbdf5ea38a9ae6768
thousandcows <milcamboo@gmail.com>
2022-09-28 16:17:11 +0900

    Knapsack question reduced time to 1/6 by reducing memory space

--- src/12865.py
+++ src/12865.py
@@ -1,36 +1,21 @@
 import sys
-from typing import List
 
 N, K = map(int, sys.stdin.readline().rstrip().split())
 
-bags = list()
-
-for _ in range(N):
-    knapsack = tuple(map(int, sys.stdin.readline().rstrip().split()))  # knapsack = (weight, value)
-    bags.append(knapsack)
+bags = list(tuple(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N))
+bags.sort()
 
 
-def pack_knapsack(bags: List[tuple]) -> int:
-    capacity = K
-    pack = []
+def pack_knapsack(capacity: int) -> int:
+    dp = [0] * (capacity + 1)
 
-    for i in range(len(bags) + 1):
-        pack.append([])
-        for c in range(capacity + 1):
-            if i == 0 or c == 0:
-                pack[i].append(0)
+    for weight, value in bags:
+        for i in range(capacity, -1, -1):
+            if i - weight < 0:
+                break
+            dp[i] = max(dp[i], value + dp[i - weight])
 
-            elif bags[i - 1][0] <= c:  # 배낭의 무게가 용량 이하인 경우
-                pack[i].append(
-                    max(
-                        bags[i - 1][1] + pack[i - 1][c - bags[i - 1][0]],
-                        # 현재 배낭의 가치와 + 현재 배낭을 담고 남은 무게에서 담을 수 있는 최대 가치
-                        pack[i - 1][c]  # dp의 이전 최댓값
-                    ))
-            else:  # 배낭의 무게가 용량을 초과하는 경우
-                pack[i].append(pack[i - 1][c])
-
-    return pack[-1][-1]
+    return dp[capacity]
 
 
-print(pack_knapsack(bags))
+print(pack_knapsack(K))
