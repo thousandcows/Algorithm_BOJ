ac6cbcf3679f898fedd7671dfb18f12b392ebf1d
thousandcows <milcamboo@gmail.com>
2022-10-06 19:26:58 +0900

    Reduced the runtime from 136ms to 108ms

--- src/1021.py
+++ src/1021.py
@@ -1,45 +1,26 @@
-from collections import deque
-
 # Read data
 N, M = map(int, input().split())
-order = list(map(int, input().split()))
-
-# Use two deque to calculate
-queue = deque()
-for i in range(1, N + 1):
-    queue.append(i)
-
-number_order = deque()
-for o in order:
-    number_order.append(o)
+queue = [int(x) for x in range(1, N + 1)]
+order_list = list(map(int, input().split()))
 
 # Variable to save counts
 count = 0
 
-while len(queue) > N - M:
-    # case 1
-    if queue[0] == number_order[0]:
-        queue.popleft()
-        number_order.popleft()
+for target in order_list:
 
+    # case 2
+    if queue.index(target) + 1 <= len(queue) - queue.index(target):
+        while queue[0] != target:
+            queue = queue[1:] + [queue[0]]
+            count += 1
+    # case 3
     else:
-        # Compare the distance from the start and the end of the queue to the target
-        target = number_order[0]
-        start_to_target_distance = queue.index(target) - 0
-        end_to_target_distance = len(queue) - 1 - queue.index(target)
+        while queue[0] != target:
+            queue = [queue[-1]] + queue[:-1]
+            count += 1
 
-        # case 2
-        if start_to_target_distance <= end_to_target_distance:
-            while queue[0] != number_order[0]:
-                new_end = queue.popleft()
-                queue.append(new_end)
-                count += 1
-        # case 3
-        else:
-            while queue[0] != number_order[0]:
-                new_start = queue.pop()
-                queue.appendleft(new_start)
-                count += 1
+    # case 1
+    queue = queue[1:]
 
 # Print answer
 print(count)
