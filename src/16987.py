6aafa014b49ca3544fe75708c7c46e9d213b69c7
thousandcows <milcamboo@gmail.com>
2022-10-06 22:16:07 +0900

    Backtracking reduced time 744ms -> 676ms

--- src/16987.py
+++ src/16987.py
@@ -14,7 +14,7 @@ def dfs(index: int, count: int) -> None:
 
     # If the egg is not broken
     if egg_list[index][0] > 0:
-        flag = True # Flag to tell if all eggs is broken except a holding egg
+        flag = True  # Flag to tell if all eggs is broken except a holding egg
         for idx in range(N):
             # Pass the same index or if the egg is already broken
 
@@ -22,19 +22,21 @@ def dfs(index: int, count: int) -> None:
                 continue
 
             flag = False
-            # Count broken eggs
-            egg_to_hit = egg_list[idx][0] - egg_list[index][1]
-            egg_to_shoot = egg_list[index][0] - egg_list[idx][1]
 
-            count_to_add = 0
-            if egg_to_hit <= 0 and egg_to_shoot <= 0:
-                count_to_add = 2
-            elif (egg_to_hit <= 0 and egg_to_shoot > 0) or (egg_to_hit > 0 and egg_to_shoot <= 0):
-                count_to_add = 1
-
+            # Hit the egg
             egg_list[idx][0] -= egg_list[index][1]
             egg_list[index][0] -= egg_list[idx][1]
+
+            # Count broken eggs
+            count_to_add = 1
+            if egg_list[idx][0] <= 0 and egg_list[index][0] <= 0:
+                count_to_add = 2
+            elif egg_list[idx][0] > 0 and egg_list[index][0] > 0:
+                count_to_add = 0
+            # DFS
             dfs(index + 1, count + count_to_add)
+
+            # Recover the egg record
             egg_list[idx][0] += egg_list[index][1]
             egg_list[index][0] += egg_list[idx][1]
 
