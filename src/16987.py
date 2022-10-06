c3205dbf52d619b36c94e45bd716d732c5ca5404
thousandcows <milcamboo@gmail.com>
2022-10-06 22:57:42 +0900

    Backtracking reduced time 640ms -> 380ms: added condition to reduce the calculation

--- src/16987.py
+++ src/16987.py
@@ -18,40 +18,37 @@ def dfs(index: int, count: int) -> None:
         ans = max(ans, count)
         return
 
-    # If the egg is not broken
-    if egg_life[index] > 0:
-        flag = True  # Flag to tell if all eggs is broken except a holding egg
-        for idx in range(N):
-            # Pass the same index or if the egg is already broken
+    # If the rest of the eggs are less than answer
+    if count + (N - index) * 2 <= ans:
+        return
 
-            if idx == index or egg_life[idx] <= 0:
-                continue
-
-            flag = False
-
-            # Hit the egg
-            egg_life[idx] -= egg_weight[index]
-            egg_life[index] -= egg_weight[idx]
-
-            # Count broken eggs
-            count_to_add = 1
-            if egg_life[idx] <= 0 and egg_life[index] <= 0:
-                count_to_add = 2
-            elif egg_life[idx] > 0 and egg_life[index] > 0:
-                count_to_add = 0
-            # DFS
-            dfs(index + 1, count + count_to_add)
-
-            # Recover the egg record
-            egg_life[idx] += egg_weight[index]
-            egg_life[index] += egg_weight[idx]
-
-        if flag:
-            ans = max(ans, N - 1)
-            return
-    # If the egg is broken
-    else:
+    # If the egg is already broken
+    if egg_life[index] <= 0:
         dfs(index + 1, count)
+        return
+
+    # If the egg is not broken
+    flag = True  # Flag to tell if all eggs is broken except a holding egg
+    for idx in range(N):
+        # Pass the same index or if the egg is already broken
+        if idx == index or egg_life[idx] <= 0:
+            continue
+
+        flag = False
+
+        # Hit the egg
+        egg_life[idx] -= egg_weight[index]
+        egg_life[index] -= egg_weight[idx]
+
+        # DFS
+        dfs(index + 1, count + int(egg_life[idx] <= 0) + int(egg_life[index] <= 0))
+
+        # Recover the egg record
+        egg_life[idx] += egg_weight[index]
+        egg_life[index] += egg_weight[idx]
+
+    if flag:
+        ans = max(ans, N - 1)
         return
 
 
