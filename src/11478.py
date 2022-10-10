3a3104885871eca8a608ef51f9ce4f7f61708bfb
thousandcows <milcamboo@gmail.com>
2022-10-10 21:14:35 +0900

    Reduced the run time: 616ms -> 568ms

--- src/11478.py
+++ src/11478.py
@@ -1,20 +1,8 @@
 s = input()
-
 word_set = set()
 
-
-def dfs(idx: int, word: str) -> None:
-    if idx > len(s) - 1:
-        return
-
-    new_word = word + s[idx]
-    word_set.add(new_word)
-
-    dfs(idx + 1, new_word)
-
-
-for i, w in enumerate(s):
-    word_set.add(w)
-    dfs(i + 1, w)
+for i in range(len(s)):
+    for j in range(i, len(s)):
+        word_set.add(s[i:j + 1])
 
 print(len(word_set))
