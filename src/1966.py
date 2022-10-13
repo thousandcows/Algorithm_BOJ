6259c37317bbd5bcbb9b9297d383ca781fcfe813
thousandcows <milcamboo@gmail.com>
2022-10-13 11:04:48 +0900

    Reduced time by removing deque and using dividing index: 148ms -> 116ms

--- src/1966.py
+++ src/1966.py
@@ -1,44 +1,31 @@
 import sys
-from collections import deque
 
 # Read test case
-test_case = int(sys.stdin.readline().rstrip())
-
-# Use deque and list
-queue = deque()
-importance_list = list()
+test_case = int(sys.stdin.readline().strip())
 
 # Calculate each test case
 for _ in range(test_case):
-    number_of_documents, target_document = map(int, sys.stdin.readline().rstrip().split())
-    document_list = map(int, sys.stdin.readline().rstrip().split())
+    number_of_documents, target_document = map(int, sys.stdin.readline().strip().split())
+    document_list = list(map(int, sys.stdin.readline().strip().split()))
 
-    for index, importance in enumerate(document_list):
-        queue.append(index)
-        importance_list.append(importance)
+    idx, top_priority, count, last = 0, max(document_list), 0, number_of_documents - 1
+    visited = [False] * number_of_documents
 
-    count = 0
-    top_priority = max(importance_list)
     while True:
 
-        curr_document = queue.popleft()
-
         # In case that current document matches the top priotiry
-        if importance_list[curr_document] == top_priority:
-
+        if not visited[idx] and document_list[idx] == top_priority:
+            visited[idx] = True
             count += 1
-            # If the document is the target
-            if curr_document == target_document:
+            last = idx
+
+            # If the target matches
+            if idx == target_document:
                 print(count)
-                queue.clear()
-                importance_list.clear()
                 break
-            # Not a target: just pop
-            else:
-                importance_list[curr_document] = -1
-                top_priority = max(importance_list)
-        # Append unmatched current document to the back of the queue
-        else:
-            queue.append(curr_document)
 
+        # Doesn't match, and searched all index without matching current top priority
+        elif idx == last:
+            top_priority -= 1
 
+        idx = (idx + 1) % number_of_documents   # Divide by the number of documents like queue works
