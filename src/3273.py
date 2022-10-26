09f76702014c3cb7a608995f54dfbe83962d544d
thousandcows <milcamboo@gmail.com>
2022-10-26 10:44:08 +0900

    Problem solving with two pointers: 148ms

--- src/3273.py
+++ src/3273.py
@@ -6,14 +6,18 @@ n = int(re())
 number_list = [int(i) for i in re().split()]
 target_number = int(re())
 
-nums_map = {}
+number_list.sort()
 ans = 0
-for i, num in enumerate(number_list):
-    nums_map[num] = i
 
-for i, num in enumerate(number_list):
-    if target_number - num in nums_map and i != nums_map[target_number - num]:
+left, right = 0, len(number_list) - 1
+
+while left != right:
+    if number_list[left] + number_list[right] < target_number:
+        left += 1
+    elif number_list[left] + number_list[right] > target_number:
+        right -= 1
+    else:
         ans += 1
+        left += 1
 
-
-print(ans // 2)
+print(ans)
