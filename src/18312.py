150290a91623fc64a3d7887dcd107f655bccab02
thousandcows <milcamboo@gmail.com>
2022-11-06 21:42:30 +0900

    Basic bruteforce algorithm: 158ms -> 136ms

--- src/18312.py
+++ src/18312.py
@@ -1,17 +1,22 @@
+# Read n, k
 n, k = map(int, input().split())
 
-cnt = 0
-target = str(k)
+# Variable for counting
+count = 0
 
-for h in range(n + 1):
-    if h < 10:
-        h = '0' + str(h)
-    for m in range(60):
-        if m < 10:
-            m = '0' + str(m)
-        for s in range(60):
-            if s < 10:
-                s = '0' + str(s)
-            if target in str(h) + str(m) + str(s):
-                cnt += 1
-print(cnt)
+for h in range(n + 1):  # hour
+    for m in range(60):     # minute
+        for s in range(60):     # second
+            # Case1: k == 0
+            if k == 0:
+                if h < 10 or m < 10 or s < 10 or h % 10 == 0 or m % 10 == 0 or s % 10 == 0:
+                    count += 1
+            # Case2: k != 0
+            elif k != 0:
+                # Check every digit of hour
+                if h // 10 == k or h % 10 == k or m // 10 == k or m % 10 == k or s // 10 == k or s % 10 == k:
+                    count += 1
+                    continue
+
+# Print count
+print(count)
