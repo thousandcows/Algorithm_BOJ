1f5baefcdbea77620d2b50629cd6929890d66120
thousandcows <milcamboo@gmail.com>
2022-11-08 22:05:22 +0900

    Problem solving with floyd warshall: 176ms

--- src/21278.py
+++ src/21278.py
@@ -1,61 +1,44 @@
 import sys
-from collections import deque
+from itertools import combinations
 
-read = sys.stdin.readline
+input = sys.stdin.readline
+INF = sys.maxsize
 
 # Read the building number and road_number
-building_number, road_number = map(int, read().split())
+building_number, road_number = map(int, input().split())
+dist_list = [[INF] * building_number for _ in range(building_number)]
 
-# Make 2 dimensional list and store graph information
-graph = [[] for _ in range(building_number + 1)]
+# Save distance information in dist_list
 for _ in range(road_number):
-    b1, b2 = map(int, read().split())
-    graph[b1].append(b2)
-    graph[b2].append(b1)
+    b1, b2 = map(int, input().split())
+    dist_list[b1 - 1][b2 - 1] = 1
+    dist_list[b2 - 1][b1 - 1] = 1
 
-# Choose Two building as chicken store and find the best option
-ans_hours, ans_chicken_one, ans_chicken_two = building_number * road_number, 0, 0
+# Use floyd warshall to save all shortest distance information
+for intermediate_vertex in range(building_number):
 
-for first_store in range(1, building_number):
-    for second_store in range(first_store + 1, building_number + 1):
+    dist_list[intermediate_vertex][intermediate_vertex] = 0
 
-        total_hours = 0
+    for start_vertex in range(building_number):
+        for end_vertex in range(building_number):
+            dist_list[start_vertex][end_vertex] = min(dist_list[start_vertex][end_vertex],
+                                                      dist_list[start_vertex][intermediate_vertex] +
+                                                      dist_list[intermediate_vertex][end_vertex])
+# Brute force to find answer
 
-        for departure_building in range(1, building_number + 1):
-            queue = deque()
-            queue.append(departure_building)
-            visited = [True] + [False for _ in range(building_number)]
-            hour_of_this_case = 0
+ans_hour = INF
+first_chicken = 0
+second_chicken = 0
 
-            while queue:
+for first_store, second_store in combinations(range(building_number), 2):
+    total_hours = 0
+    for building in range(building_number):
+        total_hours += 2 * min(dist_list[building][first_store], dist_list[building][second_store])
 
-                curr_location = queue.popleft()
-
-                # If the current location is the chicken store
-                if curr_location == first_store or curr_location == second_store:
-                    total_hours += hour_of_this_case * 2
-                    break
-
-                # If not chicken store: visit next building
-                for next_building in graph[curr_location]:
-                    if not visited[next_building]:
-                        queue.append(next_building)
-
-                hour_of_this_case += 1
-
-        # Update answers with lower time cost
-        if total_hours < ans_hours:
-            ans_chicken_one = first_store
-            ans_chicken_two = second_store
-            ans_hours = total_hours
-
-        elif total_hours == ans_hours:
-            if first_store < ans_chicken_one:
-                ans_chicken_one = first_store
-                ans_chicken_two = second_store
-            elif first_store == ans_chicken_one and second_store < ans_chicken_two:
-                ans_chicken_one = first_store
-                ans_chicken_two = second_store
+    if ans_hour > total_hours:
+        ans_hour = total_hours
+        first_chicken = first_store + 1
+        second_chicken = second_store + 1
 
 # Print answer
-print(ans_chicken_one, ans_chicken_two, ans_hours)
+print(first_chicken, second_chicken, ans_hour)
