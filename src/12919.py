b9a926cf8f313fe87e45585459ad2169aaebc544
thousandcows <milcamboo@gmail.com>
2022-11-10 13:51:59 +0900

    Bruteforce with backtracking: 108ms

--- src/12919.py
+++ src/12919.py
@@ -1,23 +1,27 @@
 # Read data
 sentence = input()
 target = input()
-target_length = len(target)
+target_length = len(sentence)
 
 
 # Function to do backtracking
 def backtracking(current_sentence: str) -> None:
     # Exit condition
     if len(current_sentence) == target_length:
-        if current_sentence == target:
+
+        if current_sentence == sentence:
             print(1)
             exit()
         else:
             return
 
-    backtracking(current_sentence + "A")  # Calculation 1
-    backtracking((current_sentence + "B")[::-1])  # Calculation 2
+    if current_sentence[-1] == "A":
+        backtracking(current_sentence[:-1])  # Calculation 1
+
+    if current_sentence[0] == "B":
+        backtracking(current_sentence[1:][::-1])  # Calculation 2
 
 
 # Do backtrack to find answer
-backtracking(sentence)
+backtracking(target)
 print(0)  # Print 0 if not possible
