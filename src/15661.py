6ce9159dd827320d72e79b815ceb8713130ec4c9
thousandcows <milcamboo@gmail.com>
2022-11-11 18:23:48 +0900

    Reduced time: 4976ms -> 1672ms

--- src/15661.py
+++ src/15661.py
@@ -4,33 +4,24 @@ from itertools import combinations
 input = sys.stdin.readline
 
 table_size = int(input())
-capability_storage = dict()
 players = [i for i in range(1, table_size + 1)]
-
-for i in range(1, table_size):
-    for j in range(i + 1, table_size + 1):
-        capability_storage[(i, j)] = 0
+capability_store = [[int(i) for i in input().split()] for _ in range(table_size)]
 
 
 def add_capability(team_members):
     total = 0
-    for first in range(len(team_members) - 1):
-        for second in range(first + 1, len(team_members)):
-            total += capability_storage[(team_members[first], team_members[second])]
+    for i in range(len(team_members) - 1):
+        for j in range(i + 1, len(team_members)):
+            first = team_members[i]
+            second = team_members[j]
+            total += capability_store[first - 1][second - 1] + capability_store[second - 1][first - 1]
 
     return total
 
 
-for row_number in range(table_size):
-    numbers = map(int, input().split())
-    for col_number, capability in enumerate(numbers):
-        if row_number < col_number:
-            capability_storage[(row_number + 1, col_number + 1)] += capability
-        elif row_number > col_number:
-            capability_storage[(col_number + 1, row_number + 1)] += capability
-
 min_value = table_size ** 2 * 100
 for start_team_count in range(1, (table_size // 2) + 1):
+
     start_team_cases = list(combinations(players, start_team_count))
 
     for start_team in start_team_cases:
