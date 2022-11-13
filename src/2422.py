da12437457da2bb5e6ff219856abff7cfae69bbb
thousandcows <milcamboo@gmail.com>
2022-11-13 13:34:04 +0900

    3-for-loop implemented: 204ms

--- src/2422.py
+++ src/2422.py
@@ -1,5 +1,4 @@
 import sys
-from itertools import combinations
 
 input = sys.stdin.readline
 
@@ -7,29 +6,28 @@ input = sys.stdin.readline
 n, cases = map(int, input().split())
 
 # Store the banned combination
-combinations_to_avoid = list()
+combinations_to_avoid = [[] for _ in range(n + 1)]
 
 for _ in range(cases):
-    new_combi = tuple(map(int, input().split()))
-    combinations_to_avoid.append(new_combi)
+    one, two = map(int, input().split())
+    combinations_to_avoid[one].append(two)
+    combinations_to_avoid[two].append(one)
 
 # Find all possible combinations
-all_possible_combinations = list(combinations([i for i in range(1, n + 1)], 3))
-
 ans = 0
 
-# Iterate all cases to see if they match the condition
-for possible_case in all_possible_combinations:
+for i in range(1, n - 1):
+    for j in range(i + 1, n):
+        if j in combinations_to_avoid[i]:
+            continue
+        for k in range(j + 1, n + 1):
+            if k in combinations_to_avoid[i]:
+                continue
 
-    count = 0
-    for case in combinations_to_avoid:
-        if case[0] in possible_case and case[1] in possible_case:
-            break
+            if k in combinations_to_avoid[j]:
+                continue
 
-        count += 1
-
-    if count == cases:
-        ans += 1
+            ans += 1
 
 # Print answer
 print(ans)
