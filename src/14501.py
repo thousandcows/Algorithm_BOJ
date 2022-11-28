cb5b625c42bfd4e508cecc7da73fa87c1da63c0a
thousandcows <milcamboo@gmail.com>
2022-11-28 14:15:11 +0900

    Reduced time complexity with dp: 196 -> 108ms

--- src/14501.py
+++ src/14501.py
@@ -3,44 +3,21 @@ import sys
 read = sys.stdin.readline
 
 # 1. Read counseling information
-number_of_days = int(read()) - 1
+number_of_days = int(read())
 
-counseling_session_list = []
+counseling_list = []
 
-for _ in range(number_of_days + 1):
-    session_info = list(map(int, read().split()))
-    counseling_session_list.append(session_info)
+for _ in range(number_of_days):
+    counseling_list.append(tuple(map(int, read().split())))
 
-# 2. Calculate maximum profit
-max_profit = 0
+# 2. Calculate maximum profit with dynamic programming
+dp_list = [0 for _ in range(20)]
 
-
-def backtracking(current_day: int, last_profit: int):
-    global max_profit
-
-    counseling_ending_time = current_day + counseling_session_list[current_day][0] - 1
-
-    # Exit condition: if the counseling ends later than the last day
-    if counseling_ending_time > number_of_days:
-        return
-
-    # Update max_profit
-    current_profit = last_profit + counseling_session_list[current_day][1]
-
-    if current_profit > max_profit:
-        max_profit = current_profit
-
-    # Scan the rest of days of counseling sessions
-    for next_day in range(counseling_ending_time + 1, number_of_days + 1):
-        backtracking(next_day, current_profit)
-
-    # End function if it's the last counseling of this case
-    return
-
-
-# Bruteforce all scenario
-for day, information in enumerate(counseling_session_list):
-    backtracking(day, 0)
+for day in range(number_of_days):
+    dp_list[day + counseling_list[day][0]] = max(
+        max(dp_list[:day + 1]) + counseling_list[day][1],
+        dp_list[day + counseling_list[day][0]]
+    )
 
 # Print answer
-print(max_profit)
+print(max(dp_list[:number_of_days + 1]))
