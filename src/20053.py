65b80c0771cdaf7b08656a5709e75b11edafe613
HYUNWOO CHEON <milcamboo@gmail.com>
2023-01-23 13:58:11 +0900

    basic implementation: 840ms

--- src/20053.py
+++ src/20053.py
@@ -6,5 +6,5 @@ n = int(input())
 
 for _ in range(n):
     length = input()
-    curr_list = list(map(int, input().split(' ')))
+    curr_list = list(map(int, input().split(' ')))  # clarifying the separator would be faster
     print(min(curr_list), max(curr_list))
