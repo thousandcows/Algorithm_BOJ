3913a56b2069324cf37d97f383bc5501987e723f
HYUNWOO CHEON <milcamboo@gmail.com>
2023-01-23 13:54:41 +0900

    basic implementation problem solving: 840ms

---
+++ src/20053.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    length = input()
+    curr_list = list(map(int, input().split(' ')))
+    print(min(curr_list), max(curr_list))
